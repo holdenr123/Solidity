35203c1124f4fc1d633072162888fb0e337f040b
holdenr123 <holdenr@hotmail.co.uk>
2021-12-20 19:52:29 +0000

    update compiler version in solidity contract

--- Hello.sol
+++ Hello.sol
@@ -1,5 +1,5 @@
 // My First Smart Contract 
-pragma solidity >=0.5.0 <0.7.0;
+pragma solidity >=0.5.0 <0.8.7;
 contract HelloWorld {
     function get()public pure returns (string memory){
         return 'Hello Contracts';
